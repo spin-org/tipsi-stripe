51255fc35a47aabc815b01463492be5fbc56295a
Anton Petrov <petrov.aap@gmail.com>
2017-04-05 14:15:20 +0300

    Add new build target

--- .travis/ci.sh
+++ .travis/ci.sh
@@ -5,8 +5,7 @@ case "${TRAVIS_OS_NAME}" in
     cd example_tmp
     npm run configure
     set -o pipefail && npm run build:ios | xcpretty -c -f `xcpretty-travis-formatter`
-    npm run test:ios -- --platform-version 8.4 --automation-name XCUITest
-    npm run test:ios -- --platform-version 10.0 --automation-name XCUITest
+    npm run test:ios
   ;;
   linux)
     cd example_tmp
