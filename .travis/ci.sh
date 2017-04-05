87ab5a45aa20d4d545d4fb09bb91b0a271f56833
Anton Petrov <petrov.aap@gmail.com>
2017-04-05 10:14:30 +0300

    Added tests for iOS 8.4

--- .travis/ci.sh
+++ .travis/ci.sh
@@ -5,7 +5,8 @@ case "${TRAVIS_OS_NAME}" in
     cd example_tmp
     npm run configure
     set -o pipefail && npm run build:ios | xcpretty -c -f `xcpretty-travis-formatter`
-    npm run test:ios
+    npm run test:ios -- --platform-version 8.4
+    npm run test:ios -- --platform-version 10.0
   ;;
   linux)
     cd example_tmp
