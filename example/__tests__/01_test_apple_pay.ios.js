223d1ec34453cad83ac3aa0f2fb98477d4e0e8c5
Dmytro Zavgorodniy <dmytro.zavgorodniy@gmail.com>
2017-04-06 09:18:50 +0700

    Add e2e tests

--- example/__tests__/01_test_apple_pay.ios.js
+++ example/__tests__/01_test_apple_pay.ios.js
@@ -1,67 +1,110 @@
+import test from 'tape-async'
 import helper from 'tipsi-appium-helper'
-import test from './utils/tape'
-import openTestSuite from './common/openTestSuite'
 
 const { driver, idFromAccessId } = helper
 
 test('Test if user can use Apple Pay', async (t) => {
+  const applePayTabId = idFromAccessId('Pay')
   const applePayButtonId = idFromAccessId('applePayButton')
   const applePaySwitchId = idFromAccessId('applePaySwitch')
   const payWithPasscodeButtonId = idFromAccessId('Pay with Passcode')
   const statusId = idFromAccessId('applePayStatus')
   const tokenId = idFromAccessId('applePayToken')
-  const setupApplePayButtonId = idFromAccessId('setupApplePayButton')
+  const deviceSupportsApplePayStatusId = idFromAccessId('deviceSupportsApplePayStatus')
+  const americanExpressAvailabilityStatusId = idFromAccessId('americanExpressAvailabilityStatus')
+  const discoverAvailabilityStatusId = idFromAccessId('discoverAvailabilityStatus')
+  const masterCardAvailabilityStatusId = idFromAccessId('masterCardAvailabilityStatus')
+  const visaAvailabilityStatusId = idFromAccessId('visaAvailabilityStatus')
 
-  await openTestSuite('Pay')
+  try {
+    await driver.waitForVisible(applePayTabId, 60000)
+    await driver.click(applePayTabId)
 
-  await driver.waitForVisible(applePayButtonId, 30000)
-  t.pass('User should see `Pay with Pay` button')
+    await driver.waitForVisible(applePayButtonId, 30000)
+    t.pass('User should see `Pay with Pay` button')
 
-  await driver.click(applePayButtonId)
-  t.pass('User should be able to tap on `Pay with Pay` button')
+    await driver.click(applePayButtonId)
+    t.pass('User should be able to tap on `Pay with Pay` button')
 
-  await driver.waitForVisible(payWithPasscodeButtonId, 30000)
-  t.pass('User should see Pay form')
+    await driver.waitForVisible(payWithPasscodeButtonId, 30000)
+    t.pass('User should see Pay form')
 
-  await driver.click(payWithPasscodeButtonId)
-  t.pass('User should accept Pay payment')
+    await driver.click(payWithPasscodeButtonId)
+    t.pass('User should accept Pay payment')
 
-  await driver.waitForVisible(tokenId, 60000)
-  t.pass('User should see token')
+    await driver.waitForVisible(tokenId, 60000)
+    t.pass('User should see token')
 
-  t.equal(
-    await driver.waitForVisible(statusId).getText(statusId),
-    'Apple Pay payment completed',
-    'Apple Pay payment should be completed'
-  )
+    t.equal(
+      await driver.waitForVisible(statusId).getText(statusId),
+      'Apple Pay payment completed',
+      'Apple Pay payment should be completed'
+    )
 
-  await driver.click(applePaySwitchId)
-  t.pass('User should be able to tap on `Complete/Cancel` switch')
+    await driver.click(applePaySwitchId)
+    t.pass('User should be able to tap on `Complete/Cancel` switch')
 
-  await driver.click(applePayButtonId)
-  t.pass('User should be able to tap on `Pay with Pay` button')
+    await driver.click(applePayButtonId)
+    t.pass('User should be able to tap on `Pay with Pay` button')
 
-  await driver.waitForVisible(payWithPasscodeButtonId, 30000)
-  t.pass('User should see Pay form')
+    await driver.waitForVisible(payWithPasscodeButtonId, 30000)
+    t.pass('User should see Pay form')
 
-  await driver.click(payWithPasscodeButtonId)
-  t.pass('User should accept Pay payment')
+    await driver.click(payWithPasscodeButtonId)
+    t.pass('User should accept Pay payment')
 
-  await driver.waitForVisible(tokenId, 60000)
-  t.pass('User should see token')
+    await driver.waitForVisible(tokenId, 60000)
+    t.pass('User should see token')
 
-  t.equal(
-    await driver.waitForVisible(statusId).getText(statusId),
-    'Apple Pay payment cenceled',
-    'Apple Pay payment should be cenceled'
-  )
+    t.equal(
+      await driver.waitForVisible(statusId).getText(statusId),
+      'Apple Pay payment cenceled',
+      'Apple Pay payment should be cenceled'
+    )
 
-  await driver.waitForVisible(setupApplePayButtonId, 30000)
-  t.pass('User should see `Setup Pay` button')
+    t.equal(
+      await driver
+      .waitForVisible(deviceSupportsApplePayStatusId)
+      .getText(deviceSupportsApplePayStatusId),
+      'Device supports Pay',
+      'Device should support Pay'
+    )
 
-  await driver.click(setupApplePayButtonId)
-  t.pass('User should be able to tap on `Setup Pay` button')
+    t.equal(
+      await driver
+      .waitForVisible(americanExpressAvailabilityStatusId)
+      .getText(americanExpressAvailabilityStatusId),
+      'American Express is available',
+      'American Express should be available'
+    )
 
-  await driver.waitForVisible(setupApplePayButtonId, 30000)
-  t.pass('User should still see `Setup Pay` button')
+    t.equal(
+      await driver
+      .waitForVisible(discoverAvailabilityStatusId)
+      .getText(discoverAvailabilityStatusId),
+      'Discover is available',
+      'Discover should be available'
+    )
+
+    t.equal(
+      await driver
+      .waitForVisible(masterCardAvailabilityStatusId)
+      .getText(masterCardAvailabilityStatusId),
+      'Master Card is available',
+      'Master Card should be available'
+    )
+
+    t.equal(
+      await driver
+      .waitForVisible(visaAvailabilityStatusId)
+      .getText(visaAvailabilityStatusId),
+      'Visa is available',
+      'Visa should be available'
+    )
+  } catch (error) {
+    await helper.screenshot()
+    await helper.source()
+
+    throw error
+  }
 })
